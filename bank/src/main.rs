0a1966bbf821d46aa66f8b9ef92149ee554de108
mhmdhalawi <mohammadhalawi93@gmail.com>
2024-12-23 04:46:01 +0200

    finalize methods

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -13,14 +13,18 @@ impl Account {
             holder,
         }
     }
-    fn deposit(&mut self, amount: i32) {
+    fn deposit(&mut self, amount: i32) -> i32 {
         self.balance += amount;
-        println!("Deposited {} to account {}-{}", amount, self.id, self.holder);
+        self.balance
     }
 
-    fn withdraw(&mut self, amount: i32) {
+    fn withdraw(&mut self, amount: i32) -> i32 {
         self.balance -= amount;
-        println!("Withdrawn {} from account {}-{}", amount, self.id, self.holder);
+        self.balance
+    }
+
+    fn summary(&self) -> String {
+        format!("Account {} with holder {} has balance {}", self.id, self.holder, self.balance)
     }
 }
 
@@ -39,21 +43,35 @@ impl Bank {
     fn add_account(&mut self, account: Account) {
         self.accounts.push(account);
     }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts
+            .iter()
+            .map(|account| account.balance)
+            .sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 fn main() {
     let mut bank = Bank::new();
 
     let mut account1 = Account::new(1, "Alice".to_string());
-    let mut account2 = Account::new(2, "Bob".to_string());
 
     account1.deposit(100);
-    account2.deposit(200);
-
     account1.withdraw(50);
 
-    bank.add_account(account1);
-    bank.add_account(account2);
+    account1.summary();
 
-    println!("{:?}", bank);
+    bank.add_account(account1);
+
+    bank.total_balance();
+
+    println!("{:?}", bank.summary());
 }
